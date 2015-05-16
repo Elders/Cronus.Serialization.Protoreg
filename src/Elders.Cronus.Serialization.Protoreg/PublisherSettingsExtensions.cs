53fa95bc96edbfc2a1cd9c707837203b8fbec710
mynkow <mynkow@gmail.com>
2015-05-16 08:52:15 +0300

    Initial release for Cronus 1.*

---
+++ src/Elders.Cronus.Serialization.Protoreg/PublisherSettingsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Elders.Cronus.DomainModeling;
+using Elders.Cronus.Serializer;
+using Elders.Cronus.Serializer.Protoreg;
+using Elders.Cronus.IocContainer;
+
+namespace Elders.Cronus.Pipeline.Config
+{
+    public static class PublisherSettingsExtensions
+    {
+        public static T UseContractsFromAssemblies<T>(this T self, Assembly[] assembliesContainingContracts, Type[] contracts = null)
+            where T : ICanConfigureSerializer
+        {
+            var builder = self as ISettingsBuilder;
+            if (builder.Container.IsRegistered<ISerializer>())
+                throw new InvalidOperationException("Protoreg serializer is already initialized.");
+
+            var protoreg = new Elders.Protoreg.ProtoRegistration();
+            protoreg.RegisterAssembly(typeof(Elders.Cronus.CronusAssembly));
+            protoreg.RegisterAssembly(typeof(IMessage));
+
+            foreach (var ass in assembliesContainingContracts)
+            {
+                protoreg.RegisterAssembly(ass);
+            }
+
+            if (contracts != null)
+                contracts.ToList().ForEach(c => protoreg.RegisterCommonType(c));
+
+            var ser = new ProtoregSerializer(protoreg);
+            ser.Build();
+            builder.Container.RegisterSingleton<ISerializer>(() => ser);
+            return self;
+        }
+    }
+}
---
+++ src/Elders.Cronus.Serialization.Protoreg/RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 1.0.0 - 15.05.2015
+* Initial release for Cronus 1.*
